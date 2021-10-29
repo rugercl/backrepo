df37a4099c32d1b4822febd001b4634d13bf5e84
Edgardo Palacios <rugercl@gmail.com>
2021-10-28 21:44:22 -0300

    first commit

---
+++ desafio7/knexfile.js
@@ -0,0 +1,11 @@
+const knex = require("knex")({
+  client: "sqlite3",
+  connection: {
+    filename: "./db/ecommerce.db3",
+  },
+  useNullAsDefault: true,
+
+  pool: { min: 2, max: 8 },
+});
+
+module.exports = knex
