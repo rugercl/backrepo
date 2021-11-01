abbf893095cb4eea0c2cd2c6934d22c9cf93c834
Edgardo Palacios <rugercl@gmail.com>
2021-11-01 15:50:40 -0300

    desafio7

--- desafio7/knexfile.js
+++ desafio7/knexfile.js
@@ -1,3 +1,18 @@
+// Update with your config settings.
+
+// module.exports = {
+
+//   development: {
+//     client: 'sqlite3',
+//     connection: {
+//       filename: "./db/ecommerce.db3",
+//     },
+//   },
+//   useNullAsDefault: true,
+//   pool:{min:2, max:8}
+
+// };
+
 const knex = require("knex")({
   client: "sqlite3",
   connection: {
@@ -9,3 +24,4 @@ const knex = require("knex")({
 });
 
 module.exports = knex
+
